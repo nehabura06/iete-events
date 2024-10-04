fe773d1f284ae6ef5f79f4ef1081491ad3f2f5e5
Neha Bura <nehabura06@gmail.com>
2024-10-04 22:12:09 +0530

    new files

---
+++ events.js
@@ -0,0 +1,12 @@
+// Scroll Animation (optional feature)
+window.addEventListener("scroll", () => {
+  const cards = document.querySelectorAll(".event-card");
+  cards.forEach((card) => {
+    const rect = card.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+      card.style.transform = "translateY(0)";
+    } else {
+      card.style.transform = "translateY(50px)";
+    }
+  });
+});
