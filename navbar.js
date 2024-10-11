9e6f83630df6907f261a9b14d33bfb43ae7228dc
Neha Bura <nehabura06@gmail.com>
2024-10-11 22:29:44 +0530

    final changes

--- navbar.js
+++ navbar.js
@@ -1,18 +1,8 @@
-<<<<<<< HEAD
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function() {
   const hamburger = document.getElementById("hamburger");
   const navLinks = document.getElementById("nav-links");
 
-  hamburger.addEventListener("click", function () {
-    navLinks.classList.toggle("active");
+  hamburger.addEventListener("click", function() {
+      navLinks.classList.toggle("active");
   });
-=======
-document.addEventListener("DOMContentLoaded", function() {
-    const hamburger = document.getElementById("hamburger");
-    const navLinks = document.getElementById("nav-links");
-
-    hamburger.addEventListener("click", function() {
-        navLinks.classList.toggle("active");
-    });
->>>>>>> 9a2ecbbd9282dfd770e018cff0895db15ef58dd6
 });
